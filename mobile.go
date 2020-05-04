61277beb548f6401c7eefa850dc6e7b1a6443128
JustinTan <mrnatande@gmail.com>
2020-05-04 23:04:54 +0800

    Add

---
+++ mobile.go
@@ -0,0 +1,33 @@
+package mobile
+
+import "github.com/gopub/types"
+
+type IntE struct {
+	Val int
+	Err *Error
+}
+
+type BoolE struct {
+	Val bool
+	Err *Error
+}
+
+type StringE struct {
+	Val string
+	Err *Error
+}
+
+type Int64E struct {
+	Val int64
+	Err *Error
+}
+
+type ImageE struct {
+	Val *types.Image
+	Err *Error
+}
+
+type Float64E struct {
+	Val float64
+	Err *Error
+}
