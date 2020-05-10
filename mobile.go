fb45a513d03c59378397d73b5b9eb1265b172638
JustinTan <mrnatande@gmail.com>
2020-05-10 12:01:39 +0800

    Add interfaces

--- mobile.go
+++ mobile.go
@@ -31,3 +31,13 @@ type Float64E struct {
 	Val float64
 	Err *Error
 }
+
+type SecretManager interface {
+	GetSecret(key string) string
+	SetSecret(key, data string) bool
+	DelSecret(key string) bool
+}
+
+type Uptimer interface {
+	Uptime() int64
+}
